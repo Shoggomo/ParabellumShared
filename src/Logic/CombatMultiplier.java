957a4bf59f1c3895b6bd051d8ab436066ff9f739
Thronetis <Thronas@yahoo.de>
2016-09-13 17:24:06 +0200

    master

---
+++ src/Logic/CombatMultiplier.java
@@ -0,0 +1,20 @@
+package Logic;
+
+import Units.Unit;
+
+/**
+ * Created by Nik on 12.09.2016.
+ */
+public class CombatMultiplier {
+
+
+    double combatMultplier;
+
+    public CombatMultiplier(Unit attacker, Unit defender){
+
+    }
+
+    public double getCombatMultplier() {
+        return combatMultplier;
+    }
+}
