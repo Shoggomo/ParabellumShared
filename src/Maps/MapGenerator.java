d39459e30d6d2c0a697489d05df8409efcbd1dca
Shoggomo <andy-hot@web.de>
2016-09-14 15:06:35 +0200

    Implemented createMapFromTemplate method

--- src/Maps/MapGenerator.java
+++ src/Maps/MapGenerator.java
@@ -1,5 +1,6 @@
 package Maps;
 
+import Etc.Terrain;
 import Maps.Templates.MapTemplate;
 
 /**
@@ -9,8 +10,27 @@ public class MapGenerator {
 
     public static GameMap createMapFromTemplate(MapTemplate template) {
         String name = template.getName();
+        Terrain[][] terrains = template.getTerrains();
+        StartType[][] startMap = template.getStartMap();
+        Tile[][] tiles = createTiles(terrains, startMap);
+        GameMap result = new GameMap(name, tiles);
+        return result;
+    }
 
-        return null;
+    private static Tile[][] createTiles(Terrain[][] terrains, StartType[][] startMap) {
+        Tile[][] result = new Tile[terrains.length][terrains[0].length];
+
+        for (int x = 0; x < terrains.length; x++) {
+            for (int y = 0; y < terrains[x].length; y++) {
+                result[x][y] = createTile(terrains[x][y], startMap[x][y]);
+            }
+        }
+        return result;
+    }
+
+    private static Tile createTile(Terrain terrain, StartType startType) {
+        Tile result = new Tile(terrain, startType);
+        return result;
     }
 
 }
--- src/Maps/Templates/TestMap.java
+++ src/Maps/Templates/TestMap.java
@@ -11,12 +11,12 @@ public class TestMap extends MapTemplate {
         public TestMap() {
 
         name = "TestMap";
-        terrains = new Terrain[][]{
+                terrains = new Terrain[][]{     //Must be Rectangle
                 {M, F, S},
                 {P, P, P},
                 {R, S, W}
         };
-        startMap = new StartType[][]{
+                startMap = new StartType[][]{     //Must be Rectangle same size as Terrain
                 {NP, P1, NP},
                 {NP, NP, NP},
                 {NP, P2, NP}
