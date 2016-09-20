57771bc74cf61cc2b53c2094dbe401661e75774a
Shoggomo <andy-hot@web.de>
2016-09-20 12:55:45 +0200

    init

---
+++ src/Net/NetConstants.java
@@ -0,0 +1,9 @@
+package Net;
+
+/**
+ * Created by Shoggomo on 20.09.2016.
+ */
+public class NetConstants {
+    public static int NET_PORT = 98765;
+    public static Class[] REGISTERED_CLASSES = new Class[]{NetPacket.class};
+}
