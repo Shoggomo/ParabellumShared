373b535f99cf6313965d2cad2f99dce913a0106c
Shoggomo <andy-hot@web.de>
2016-09-22 15:27:46 +0200

    added Response and Request classes

--- src/Net/NetConstants.java
+++ src/Net/NetConstants.java
@@ -6,5 +6,16 @@ package Net;
 public class NetConstants {
     public static int NET_PORT = 2345;
     public static String NET_ADDRESS = "127.0.0.1";  //TODO
-    public static Class[] REGISTERED_CLASSES = new Class[]{NetPacket.class};
+    public static Class[] REGISTERED_CLASSES = new Class[]{
+            NetGameListPacketRequest.class,
+            NetGameListPacketResponse.class,
+            NetEnterGameRequest.class,
+            NetEnterGameResponse.class,
+            NetNewGameRequest.class,
+            NetNewGameResponse.class,
+            NetStartGameRequest.class,
+            NetStartGameResponse.class,
+            NetReadyToStartRequest.class,
+            NetReadyToStartResponse.class
+    };
 }
