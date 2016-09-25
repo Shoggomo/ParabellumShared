a8d678924eda38f520851f0182f4812040ab6562
Jakob <jakob.schoenicke@arcor.de>
2016-09-25 21:08:16 +0200

    Added enum with unit names builded .jar

---
+++ src/Units/UnitNames.java
@@ -0,0 +1,21 @@
+package Units;
+
+/**
+ * Created by Jakob on 25.09.2016.
+ */
+public enum UnitNames {
+
+    Arquebuser,
+    Cuirassier,
+    Dragonier,
+    Field_Cannon,
+    Helbadier,
+    Hussar,
+    Lancer,
+    Longbowman,
+    Man_at_Arms,
+    Mortar,
+    Musketeer,
+    Pikeman,
+
+}
